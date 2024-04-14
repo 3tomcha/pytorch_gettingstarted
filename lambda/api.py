002d2d3a738652c65927174834885381ede96d21
Tomoya Kobayashi <tomocha.marika@gmail.com>
2024-04-14 11:31:42 +0900

    install package

--- lambda/api.py
+++ lambda/api.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import torch
+import joblib
+from torch import nn
+import numpy as np
 
 app = Flask(__name__)
 CORS(app)
